08903d11a8263f073bef93823c8fb0e4428b3b19
rbhan29 <162993881+rbhan29@users.noreply.github.com>
2024-03-13 16:32:21 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,5 +1,4 @@
-#  ALL REGISTERS
-
+# REGISTERS
 regdict = {
     "zero": "00000",
     "ra": "00001",
@@ -35,10 +34,8 @@ regdict = {
     "t6": "11111",
 }
 
-# Instruction = { opname : (opcode, funct3, funct7)}
-
-Instruction = {
-
+instruction = {
+    
     # R-type
     "add": ("0110011", "000", "0000000"),
     "sub": ("0110011", "000", "0100000"),
@@ -49,13 +46,16 @@ Instruction = {
     "srl": ("0110011", "101", "0000000"),
     "or": ("0110011", "110", "0000000"),
     "and": ("0110011", "111", "0000000"),
-
+    
     # I-type
     "lw": ("0000011", "010"),
     "addi": ("0010011", "000"),
     "sltiu": ("0010011", "011"),
     "jalr": ("1100111", "000"),
-
+    
+    # S-type
+    "sw": ("0100011", "010"),
+    
     # B-type
     "beq": ("1100011", "000"),
     "bne": ("1100011", "001"),
@@ -63,59 +63,24 @@ Instruction = {
     "bge": ("1100011", "101"),
     "bltu": ("1100011", "110"),
     "bgeu": ("1100011", "111"),
-
-    # S-type
-    "sw": ("0100011", "010"),
-
+    
     # U-type
     "lui": ("0110111"),
     "auipc": ("0010111"),
-
+    
     # J-type
-    "jal": ("0010111"),
+    "jal": ("1101111")
 }
 
-Rtype = ["add", "sub", "sll", ",slt", "sltu", "xor", "srl", "or", "and"]
-Itype = ["lw", "addi", "sltiu", "jalr"]
-Btype = ["blt", "bge", "bltu", "bgeu", "bne", "beq"]
-Stype = ["sw"]
-Utype = ["auipc", "lui"]
-Jtype = ["jal"]
-Alltype = Rtype + Itype + Stype + Btype + Utype + Jtype
+Rtype =["add","sub","sll",",slt","sltu","xor","srl","or","and"]
+Itype=["lw","addi","sltiu","jalr"]
+Stype=["sw"]
+Btype=["blt","bge","bltu","bgeu","bne","beq"]
+Utype=["auipc","lui"]
+Jtype=["jal"]
+all_type=Rtype+Itype+Stype+Btype+Utype+Jtype
 
-def outofbounds(inst, imm):
-    if inst in Utype or inst in Jtype:
-        if len(imm) > 12:
-            print("immediate out of bounds")
-            return False
-        return True
-    else:
-        if len(imm) > 20:
-            print("immediate out of bounds")
-            return False
-        return True
-       
-def typo_inst(inst):
-    if inst not in Alltype:
-        print("Error in Instruction name ", inst)
-        return False
-    return True
-
-def typo_reg(reg):
-    if reg not in regdict.keys():
-        print("Error in register name ", reg)
-        return False
-    return True
-
-
-def missing_halt(all_inst):
-    halt = ["beq", "zero", "zero", "0"]
-    for i in all_inst:
-        if all_inst[i] == halt:
-            return True
-    print("Error: missing halt Instruction")
-    return False
-    def parseForInstructions(file):
+def parseForInstructions(file):
     instructions = {}
 
     with open(file, "r") as f:
@@ -135,7 +100,6 @@ def missing_halt(all_inst):
 
     return instructions
 
-
 def get_labels(file):
     labels = {}
 
@@ -152,13 +116,32 @@ def get_labels(file):
 
     return labels
 
+def typo_inst(inst):   
+    if(inst not in all_type):
+            print("Error in instruction name ", inst) 
+            return False   
+    return True
     
+def typo_reg(reg):
+    if(reg not in regdict.keys()):
+            print("Error in register name ", reg)
+            return False
+    return True
+
+# retrun error if not even a single halt instruction is present
+def missing_halt(all_inst):
+    halt = ["beq", "zero", "zero", "0"]
+    for i in all_inst:
+        if all_inst[i] == halt:
+            return True
+    print("Error: missing halt instruction")
+    return False
+
 def dec_to_twocomp(decimal, bits):
     if decimal < 0:
         decimal = (1<<bits) + decimal
     format_string = '{:0%ib}' % bits
     return format_string.format(decimal)
-    
 
 def dec_to_bin_12(decimal):
     binary = bin(int(decimal))[2:] 
@@ -172,33 +155,44 @@ def dec_to_bin_20(decimal):
 
 def decimal_to_binary_twos_complement(decimal_num, num_bits):
     # Handle negative numbers
-    if decimal_num < 0:
-        decimal_num += 2 ** num_bits
+    new_dec=int(decimal_num)
+    if new_dec < 0:
+        new_dec += 2 ** num_bits
 
-    binary = bin(decimal_num)[2:].zfill(num_bits)
+    binary = bin(new_dec)[2:].zfill(num_bits)
     return binary
-
+ 
 def typeR(ins,rd,rs1,rs2):
     if(typo_reg(rd) is True and typo_reg(rs1) is True and typo_reg(rs2) is True):
         if(typo_inst(ins)):
-            print(instruction[ins][2]+reg_file[rs2]+reg_file[rs1]+instruction[ins][1]+reg_file[rd]+instruction[ins][0])
+            print(instruction[ins][2]+regdict[rs2]+regdict[rs1]+instruction[ins][1]+regdict[rd]+instruction[ins][0])
         else:
             exit()
     else:
         exit()
-        
+
+def typeI(ins,rd,rs1,imm):
+    k=dec_to_bin_12(imm)
+    if(typo_reg(rd) is True and typo_reg(rs1) is True):
+        if(typo_inst(ins)):
+            print(k+regdict[rs1]+instruction[ins][1]+regdict[rd]+instruction[ins][0])
+        else:
+            exit()      
+    else:
+        exit()
+
 def typeS(ins,rd,imm,rs1):
     bin = dec_to_bin_12(imm)
     x = bin[0:5][::-1]
     y = bin[6:12][::-1]
     if(typo_reg(rd) is True and typo_reg(rs1) is True ):
         if(typo_inst(ins)):
-            print(y,reg_file[rd],reg_file[rs1],instruction[ins][1],x,instruction[ins][0])
+            print(y,regdict[rd],regdict[rs1],instruction[ins][1],x,instruction[ins][0])
         else:
             exit()
     else:
         exit()
-        
+      
 def typeU(ins,rd,imm):
     bin = decimal_to_binary_twos_complement(imm, 32)
     bin = bin[::-1]
@@ -206,11 +200,170 @@ def typeU(ins,rd,imm):
     
     if(typo_reg(rd) is True):
         if(typo_inst(ins)):
-            print(bin,reg_file[rd],instruction[ins][0])
+            print(bin,regdict[rd],instruction[ins][0])
         else:
             exit()
     else:
         exit()
+        
+def typeB(ins, rs1, rs2, lbl,pc):
+
+    if ins=="beq" and rs1=="zero" and rs2=="zero" and lbl=="0":
+        print("00000000000000000000000001100011")
+        return
+    
+    if isinstance(lbl, int):
+        imm = decimal_to_binary_twos_complement(lbl,32)[-2:-22:-1][-1::-1]
+        w = imm[0]
+        x = imm[-1:-11:-1][-1::-1]
+        y = imm[-11]
+        z = imm[1:9]
+        print(imm)
+        if typo_reg(rs1) is True and typo_reg(rs2) is True:
+            if typo_inst(ins):
+                print(
+                str(w)
+                + str(x)
+                + regdict[rs2]
+                + regdict[rs1]
+                + instruction[ins][1]
+                + str(y)
+                + str(z)
+                + instruction[ins][0]
+                )
+             
+    elif(lbl in labels):
+        # getting the line number of the label
+        lbl_pc = labels[lbl]
+        # getting the current line number of the instruction
+        current_pc = pc
+        # calculating the offset
+        offset = (current_pc-lbl_pc) * 4
+        
+        imm = decimal_to_binary_twos_complement(offset,20)[7:19]
+        w = imm[0]
+        x = imm[2:8]
+        y = imm[8::]
+        z = imm[1]
+
+        if typo_reg(rs1) is True and typo_reg(rs2) is True:
+            if typo_inst(ins):
+                print(
+                    str(w)
+                    + str(x)
+                    + regdict[rs2]
+                    + regdict[rs1]
+                    + instruction[ins][1]
+                    + str(y)
+                    + str(z)
+                    + instruction[ins][0]
+                )
+            else:
+                exit()
+       
+def typeJ(ins, rd, lbl,pc):
+    print(isinstance(lbl, int))
+    if lbl in labels:
+        lbl_pc = labels[lbl]
+        # getting the current line number of the instruction
+        current_pc = pc
+        # calculating the offset
+        offset = (current_pc-lbl_pc) * 4        
+        imm = decimal_to_binary_twos_complement(offset,32)[-2:-22:-1][-1::-1]
+        print(imm)
+
+        w = imm[0]
+        x = imm[-1:-11:-1][-1::-1]
+        y = imm[-11]
+        z = imm[1:9]
+
+        if typo_reg(rd) is True:
+            if typo_inst(ins):
+                print(
+                    str(w)
+                    + str(x)
+                    + str(y)
+                    + str(z)
+                    + regdict[rd]
+                    + instruction[ins]
+                )
+    elif isinstance(lbl, int):
+        imm = decimal_to_binary_twos_complement(lbl,32)[-2:-22:-1][-1::-1]
+        w = imm[0]
+        x = imm[-1:-11:-1][-1::-1]
+        y = imm[-11]
+        z = imm[1:9]
+        print(imm)
+        if typo_reg(rd) is True:
+            if typo_inst(ins):
+                print(
+                    str(w)
+                    + str(x)
+                    + str(y)
+                    + str(z)
+                    + regdict[rd]
+                    + instruction[ins]
+                )
+    else:
+        print("Error: label not found")
+        exit()
+      
+def outofbounds(inst,imm):
+    if(inst in Utype or inst in Jtype):
+        if(len(imm)>12):
+            print("immediate out of bounds")
+            return False
+        return True
+    else:
+        if(len(imm)>20):
+            print("immediate out of bounds")
+            return False
+        return True
+   
+def line(pc):
+    if(pc>64):
+        print("Line count exceeded 64")
+        return False
+    return True
+
+import os
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python script.py folder_path")
+    sys.exit(1)
+
+# Get the folder path from the command-line argument
+folder_path = sys.argv[1]
+
+
+# List all files in the folder
+files = os.listdir(folder_path)
+
+# Iterate over each file in the folder
+for file_name in files:
+    # Construct the full path of the file
+    file_path = os.path.join(folder_path, file_name)
+    instruct = parseForInstructions(file_path)
+    labels = get_labels(file_path)
+    print(instruct)
+    print(labels)
+    for i in instruct:
+        if(instruct[i][0] in Rtype):
+            typeR(instruct[i][0],instruct[i][1],instruct[i][2],instruct[i][3])
+        elif(instruct[i][0] in Itype):
+            if(len(instruct[i])==4):
+                typeI(instruct[i][0],instruct[i][1],instruct[i][2],instruct[i][3])
+            else:
+                k=instruct[i][2].split('(')
+                typeI(instruct[i][0],instruct[i][1],k[1][0:-1],k[0])
+        elif(instruct[i][0] in Stype):
+            typeS(instruct[i][0],instruct[i][1],instruct[i][2],instruct[i][3])
+        elif(instruct[i][0] in Btype):
+            typeB(instruct[i][0],instruct[i][1],instruct[i][2],instruct[i][3],i)
+        elif(instruct[i][0] in Utype):
+            typeU(instruct[i][0],instruct[i][1],instruct[i][2])
+        elif(instruct[i][0] in Jtype):
+            typeJ(instruct[i][0],instruct[i][1],instruct[i][2],i)
 
 
 
